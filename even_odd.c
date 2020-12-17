3384e8d76ce108967cc820e85e2b01215f6a5791
Rashikagarg <73247928+Rashikagarg@users.noreply.github.com>
2020-12-17 23:32:56 +0530

    Add files via upload

---
+++ even_odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    /* code */int a;
+    printf("Enter the no. between 1-100");
+    scanf("%d",&a);
+    if (a%2==0)
+    {
+        /* code */
+        printf("No. is even");
+    }
+    else
+    printf("/n no. is odd");
+    
+    return 0;
+}
