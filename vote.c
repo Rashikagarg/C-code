9643178a60a945b8e3bfdd6fd89f95bb33a32b52
Rashikagarg <73247928+Rashikagarg@users.noreply.github.com>
2020-12-18 23:20:18 +0530

    Add files via upload

---
+++ vote.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    /* code */
+    int a;
+    printf("Enter the value of candidate:");
+    scanf("%d",&a);
+    if (a>=18)
+    {
+        /* code */
+        printf("you are eligible for voting");
+    }
+    else
+    printf("you are not eligible for voting");
+    
+    return 0;
+}
