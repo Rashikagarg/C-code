a01233396539e27a25c5de41e1a5eb4f076b3254
Rashikagarg <73247928+Rashikagarg@users.noreply.github.com>
2020-12-21 23:37:32 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,12 @@
+#include<stdio.h>  //headerfile include:preprocessor directive
+int main()
+{
+    /* code */
+    printf("Hello World");
+    //input -> process -> output // principle of computer
+    
+    return 0;
+}
+//function is block of code, which have a name and can be execute again and again,
+//It may be possible to pass arguments to function and it can return value if require.
+                                                                                                                                                                                                                                                                                                                                                                                                       
